94ba45f5650e30be240a7bb49aa0e67760614ef8
DynamikArray <brian@monumentixwebdesign.com>
2019-01-01 05:14:51 -0500

    UPDATED: changed parm command to be a rich embed with author info

--- commands/ParmCommand.js
+++ commands/ParmCommand.js
@@ -15,6 +15,42 @@ class ParmCommand extends Command {
     const parm = this.client.channels.get(parmChannelId);
 
     parm.fetchMessages({ limit: 100 }).then(messages => {
+      parm
+        .fetchMessages({ limit: 100, before: messages.lastKey() })
+        .then(msgs => {
+          const manyMessages = messages.concat(msgs);
+          const msg = manyMessages.random();
+          //
+          const author = msg.author.username;
+          const thumbnail = msg.author.avatarURL;
+          const attachment = msg.attachments.random();
+
+          //
+          if (!attachment) return false;
+          if (!attachment.url) return false;
+
+          return message.channel.send(
+            this.client.util.embed({
+              color: 0x82ad64,
+              title: "Enjoy the tasty memes!",
+              thumbnail: {
+                url: `${thumbnail}`
+              },
+              description: `Posted By: ${author} `,
+              image: {
+                url: `${attachment.url}`
+              },
+              timestamp: new Date(),
+              footer: {
+                text: "Drone Disorder 2018 | DynamikArray "
+              }
+            })
+          );
+        });
+    });
+
+    /*
+    parm.fetchMessages({ limit: 100 }).then(messages => {
       const msg = messages.random();
       const attachment = msg.attachments.random();
 
@@ -23,6 +59,7 @@ class ParmCommand extends Command {
 
       return message.reply(`${attachment.url}`);
     });
+    */
   }
 }
 
