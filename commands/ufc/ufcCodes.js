78ee9dee37bb8f841c0a58a5b3b35e363defde6b
DynamikArray <DynamikArray@gmail.com>
2020-06-03 19:53:03 -0400

    FIXED: Weight class codes

--- commands/ufc/ufcCodes.js
+++ commands/ufc/ufcCodes.js
@@ -1,36 +1,70 @@
 const fightTypes = {
-  "403": {
-    name: "Women's Flyweight"
-  },
-  "22": {
-    name: "Middleweight"
-  },
-  "19": {
-    name: "Welterweight"
+  "970": {
+    name: "Bantamweight",
   },
   "20": {
-    name: "Bantamweight"
+    name: "Bantamweight",
   },
-  "6": {
-    name: "Flyweight "
+
+  "994": {
+    name: "Catchweight",
   },
-  "32": { 
-    name: "Heavyweight"
-  },
-  "36": {
-    name: "Lightweight "
-  },
-  "40": {
-    name: "Light Heavyweight"
-  },
-  "402": {
-    name: "Women's Strawweight"
+
+  "999": {
+    name: "Featherweight",
   },
   "49": {
-    name: "Featherweight"
+    name: "Featherweight",
   },
-  "141":{
-    name: "Women's Bantamweight"
+
+  "956": {
+    name: "Flyweight ",
+  },
+  "6": {
+    name: "Flyweight ",
+  },
+
+  "990": {
+    name: "Light Heavyweight",
+  },
+  "40": {
+    name: "Light Heavyweight",
+  },
+
+  "972": {
+    name: "Middleweight",
+  },
+  "22": {
+    name: "Middleweight",
+  },
+
+  "969": {
+    name: "Welterweight",
+  },
+  "19": {
+    name: "Welterweight",
+  },
+
+  "1004": {
+    name: "Women Featherweight",
+  },
+  "403": {
+    name: "Women's Flyweight",
+  },
+
+  "32": {
+    name: "Heavyweight",
+  },
+  "36": {
+    name: "Lightweight ",
+  },
+
+  "402": {
+    name: "Women's Strawweight",
+  },
+
+  "141": {
+    name: "Women's Bantamweight",
   },
 };
 
